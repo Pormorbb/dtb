0cf44058de6c3b08e583083808405162f660ba4b
Pormorbb <panlapasp@gmail.com>
2024-09-22 23:04:04 +0700

    Update Code.R

--- r-prog/Code.R
+++ r-prog/Code.R
@@ -1,3 +1,39 @@
-hello <- function () {
-  print("Hello world")
-  }
+## classification
+
+library(tidyverse)
+library(caret)
+library(mlbench)
+
+data("PimaIndiansDiabetes") ##load data again
+
+df <- PimaIndiansDiabetes
+
+## check / inspect data null data clean
+sum(complete.cases(df)) ##count missiong values
+mean(complete.cases(df)== 1)
+mean(complete.cases(df)) ==1
+glimpse(df)
+
+## logistic regreesion ='glm'
+set.seed(42)
+
+ctrl <- trainControl (method = "cv",
+                      number = 5)
+glm_model <- train(diabetes ~ age + glucose + pressure,
+             data = df, 
+             method = "glm",
+             trControl = ctrl)
+glm_model$finalModel
+
+## variable importance
+varImp(glm_model)
+
+## regression => high bias
+## data cheahe => model doesn't change that much
+
+##save model .RDS
+saveRDS(glm_model, "Logistics_reg.RDS")
+
+##friend's Computer
+model <- readRDS("Logistics_reg.RDS")
+readRDS
